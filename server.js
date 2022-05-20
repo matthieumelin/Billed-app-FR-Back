893c7c637312ff43b400f439b4d32655899bfbfb
Matthieu Melin <matthieumelin62@gmail.com>
2022-05-20 20:39:34 +0200

    first commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 app.listen(5678, () => {
   // eslint-disable-next-line no-console
-  console.log('Example app listening on port 5678!');
+  console.log('App listening on port 5678!');
 });
